bdff815b7aca51f105d800af62b37877e9414eba
Jack Q <QiaoBo@outlook.com>
2016-12-05 21:12:59 +0800

    updata

--- 01-signal-alarm/receiver.c
+++ 01-signal-alarm/receiver.c
@@ -165,7 +165,7 @@ int main(int argc, char **argv) {
       break;
     }
 
-    if (send_addr_set && (ackCount > (int)(file->size - file->received) + 50 || ackCount < 0)) {
+    if (send_addr_set && (ackCount > (file ? (int)(file->size - file->received) : 0) + 50 || ackCount < 0)) {
       sendAck();
       ackCount = 0;
     }
